0b4a7d7ebf4bea0284c606a3530da8c9215f12dc
Bekzod-Toxtamuratov <bekzodtoxtamuratov@gmail.com>
2025-12-14 22:26:32 +0500

    test-2

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-
+const  variable=<h1>Bekzod Toxtamuratov</h1>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {variable}
   </StrictMode>,
 )
